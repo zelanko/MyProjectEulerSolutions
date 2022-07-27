8778bb11322db74b50702f5f23c48d11e1add10c
Ari Zelanko <zelanko@yahoo.com>
2022-07-26 20:27:27 -0400

    Remove loop and time performance.

--- Project_0001/SumMultiplesOf.py
+++ Project_0001/SumMultiplesOf.py
@@ -7,12 +7,12 @@ https://projecteuler.net/problem=1
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000."""
-
+import time
+start = time.perf_counter_ns()
 multiples = set(range(3, 1000, 3)) | set(range(5, 1000, 5))
-
-accumulator = 0
-
-for i in multiples:
-  accumulator += i
-
-print(accumulator)
+sum = sum(multiples)
+summed = time.perf_counter_ns()
+print("Sum of specified multiples: {0}".format(sum))
+printed = time.perf_counter_ns()
+total = printed - start
+print("Performance(ns):\n\tCalc: {0}\n\tPrint: {1}\n\tTotal: {2} ({3} s)".format(summed - start, printed-summed, total, total/1000000000))
