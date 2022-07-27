5dcce71c3969c90eb3e0d20902e4aabdd9ed0cac
Ari Zelanko <zelanko@yahoo.com>
2022-07-27 15:20:57 -0400

    Message and performance timer.

--- Project_0003/LargestPrimeFactor.py
+++ Project_0003/LargestPrimeFactor.py
@@ -7,9 +7,12 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?"""
 
 from pyprimes import factors
-
+import time
+start = time.perf_counter_ns()
 number = 600851475143
 
-factors = factors(number)
+largest_prime_factor = max(factors(number))
+end = time.perf_counter_ns()
+message = "Greatest prime factor of '{0:,d}': '{1:,d}'\nElapsed: {2:,d} ns".format(number, largest_prime_factor, end - start)
 
-print(max(factors))
+print(message)
